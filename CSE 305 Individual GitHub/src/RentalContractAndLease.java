751d1963be63c50dccd9dbd07af51ba79746aad2
tranvutruonghuy <167849255+tranvutruonghuy@users.noreply.github.com>
2024-04-23 16:06:09 +0700

    Ver 0.4

--- CSE 305 Individual GitHub/src/RentalContractAndLease.java
+++ CSE 305 Individual GitHub/src/RentalContractAndLease.java
@@ -4,7 +4,6 @@ public class RentalContractAndLease {
     private String TenantID;
     private String RentAmount;
 
-
     public RentalContractAndLease(String contractID, String propertyID, String tenantID, String rentAmount) {
         ContractID = contractID;
         PropertyID = propertyID;
@@ -12,37 +11,42 @@ public class RentalContractAndLease {
         RentAmount = rentAmount;
     }
 
-    public void createContract(){
+    public void createContract() {
 
     }
 
-    public void terminateContract(){
+    public void terminateContract() {
 
     }
-
-
 
     public String getContractID() {
         return ContractID;
     }
+
     public void setContractID(String contractID) {
         ContractID = contractID;
     }
+
     public String getPropertyID() {
         return PropertyID;
     }
+
     public void setPropertyID(String propertyID) {
         PropertyID = propertyID;
     }
+
     public String getTenantID() {
         return TenantID;
     }
+
     public void setTenantID(String tenantID) {
         TenantID = tenantID;
     }
+
     public String getRentAmount() {
         return RentAmount;
     }
+
     public void setRentAmount(String rentAmount) {
         RentAmount = rentAmount;
     }
