33a544655ffc05e4c78e47928c86051c073f54d2
tranvutruonghuy <167849255+tranvutruonghuy@users.noreply.github.com>
2024-04-23 14:33:54 +0700

    version 0.0

---
+++ CSE 305 Individual GitHub/src/Property.java
@@ -0,0 +1,6 @@
+public class Property {
+    private String propertyID;
+    private String description;
+    private String PropertyOwnerId;
+
+}
---
+++ CSE 305 Individual GitHub/src/User.java
@@ -0,0 +1,50 @@
+public class User {
+    private String userID;
+    private String password;
+    private String email;
+    private String userType;
+
+    public User(String userID, String password, String email, String userType) {
+        this.userID = userID;
+        this.password = password;
+        this.email = email;
+        this.userType = userType;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUserType() {
+        return userType;
+    }
+
+    public void setUserType(String userType) {
+        this.userType = userType;
+    }
+
+    public void createUser() {
+        user(userID, password, email, userType);
+    }
+
+}
